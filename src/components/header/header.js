f9fafc2cfc8fef74fa2f9aef77186953a89d658f
ayushakes <ayu.ayu.sharma@gmail.com>
2022-02-12 17:40:46 +0530

    fixed nav changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Nav from "./nav";
 
 const Header = () => {
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <Nav />
         <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             Button
